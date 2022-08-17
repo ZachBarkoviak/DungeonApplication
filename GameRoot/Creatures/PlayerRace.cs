5bfb0f20d48f9fa4d8d13f17a855b3a97e465dd8
ZachBarkoviak <zjbarkoviak@outlook.com>
2022-08-17 17:05:03 -0500

    adjusted code to impliment enums for chacter race, character class, and weapon type. various other bug fixes

---
+++ GameRoot/Creatures/PlayerRace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DungeonLibrary
+{
+    public enum PlayerRace
+    {
+        Elf = 1,
+        Orc = 2,
+        Human = 3,
+        Goblin = 4,
+        Tiefling = 5,
+    }
+}
